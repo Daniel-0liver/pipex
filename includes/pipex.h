16048db172ed884c6724a9ecfedf37519082bcce
Daniel-0liver <dtoliverbr@gmail.com>
2022-06-29 02:43:46 +0100

    testing

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,22 +6,21 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 23:02:10 by dateixei          #+#    #+#             */
-/*   Updated: 2022/06/13 23:26:35 by dateixei         ###   ########.fr       */
+/*   Updated: 2022/06/29 02:36:23 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include "libft/include/libft.h"
+// # include "libft/include/libft.h"
 # include <stdio.h>
+# include <stdlib.h>
 # include <unistd.h>
 # include <string.h>
 # include <fcntl.h>
 # include <errno.h>
 # include <sys/types.h>
 # include <sys/wait.h>
-
-
 
 #endif
--- pipex.c
+++ pipex.c
@@ -6,16 +6,18 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 00:22:44 by dateixei          #+#    #+#             */
-/*   Updated: 2022/06/28 01:19:28 by dateixei         ###   ########.fr       */
+/*   Updated: 2022/06/29 02:41:41 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+#include "includes/pipex.h"
 
-int	main(int argc, char **argv, char **envp)
+int main(int argc, char **argv, char **envp)
 {
-	(void)envp;
-	if (argc > 1)
-		(void) argv;
-	return (0);
+    char *options[3] = {"ls", "-la", NULL};
+
+    (void)argc;
+    (void)argv;
+    execve("/usr/bin/ls", options, envp);
+    return (0);
 }
