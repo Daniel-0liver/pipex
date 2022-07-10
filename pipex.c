af0969f714cacca666cbac901d57708639652010
Daniel-0liver <dtoliverbr@gmail.com>
2022-07-10 23:18:15 +0100

    Let's do it

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 00:22:44 by dateixei          #+#    #+#             */
-/*   Updated: 2022/07/08 00:55:46 by dateixei         ###   ########.fr       */
+/*   Updated: 2022/07/10 23:18:01 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,5 @@
 
 int main(int argc, char **argv, char **envp)
 {
-	I don't know how to start it.
 }
 
