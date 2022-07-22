f34394726dfe0aa6832d670a0c28e086c02953ae
Daniel-0liver <dtoliverbr@gmail.com>
2022-07-22 22:17:36 +0100

    Testing pratical pipe and fork

--- src/pipex.c
+++ src/pipex.c
@@ -6,29 +6,58 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 00:22:44 by dateixei          #+#    #+#             */
-/*   Updated: 2022/07/18 01:21:49 by dateixei         ###   ########.fr       */
+/*   Updated: 2022/07/22 22:16:46 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-int main(int argc, char **argv)
+int main()
 {
-	int id;
-	// int n;
-	// int i;
-	
-	(void) argc;
-	(void) argv;
-	id = 0;
-	id = fork();
+	int array[] = {1, 2, 3, 4, 1, 2};
+	int arrSize = sizeof(array) / sizeof(int);
+	int start, end;
+	int fd[2];
+	if (pipe(fd) == -1)
+		return (1);
+
+	int id = fork();
+	if (id == -1)
+		return (2);
 	if (id == 0)
-		sleep(1);
-	printf("ID :%d, %d\n", getpid(), id);
-	int n = wait(NULL);
-	if (n == -1)
-		puts("Execution finished");
-	else
-		printf("Child finished, %d", n);
+	{
+		start = 0;
+		end = start + arrSize / 2;
+	} else 
+	{
+		start = arrSize / 2;
+		end = arrSize;
+	}
+
+	int sum = 0;
+	int i;
+	for (i = start; i < end; i++)
+		sum += array[i];
+
+	wait(NULL);
+	printf("Calculated partial sum: %d\n", sum);
+	
+	if (id == 0)
+	{
+		close(fd[0]);
+		write(fd[1], &sum, sizeof(sum));
+		close(fd[1]);	 
+	} 
+		else 
+	{
+		int sumFromChild;
+		close(fd[1]);
+		read(fd[0], &sumFromChild, sizeof(sumFromChild));
+		close(fd[0]);
+
+		int totalSum = sum + sumFromChild;
+		printf("Total sum is %d\n", totalSum);
+	}
+	
 	return (0);
 }
