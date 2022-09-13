a42e635d24edb8867355015de01b5be94bef48a3
Daniel-0liver <dtoliverbr@gmail.com>
2022-09-13 22:37:29 +0100

    Cube terminal

--- src/pipex.c
+++ src/pipex.c
@@ -6,60 +6,175 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 00:22:44 by dateixei          #+#    #+#             */
-/*   Updated: 2022/09/12 22:40:00 by dateixei         ###   ########.fr       */
+/*   Updated: 2022/09/13 22:28:31 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/pipex.h"
+// #include "../includes/pipex.h"
 
-int main()
-{
-	int array[] = {1, 2, 3, 4, 1, 2};
-	int arrSize = sizeof(array) / sizeof(int);
-	int start, end;
-	int fd[2];
-	if (pipe(fd) == -1)
-		return (1);
+// int main()
+// {
+// 	int array[] = {1, 2, 3, 4, 1, 2};
+// 	int arrSize = sizeof(array) / sizeof(int);
+// 	int start, end;
+// 	int fd[2];
+// 	if (pipe(fd) == -1)
+// 		return (1);
 
-	int id = fork();
-	if (id == -1)
-		return (2);
-	if (id == 0)
-	{
-		start = 0;
-		end = start + arrSize / 2;
-	} else 
-	{
-		start = arrSize / 2;
-		end = arrSize;
-	}
+// 	int id = fork();
+// 	if (id == -1)
+// 		return (2);
+// 	if (id == 0)
+// 	{
+// 		start = 0;
+// 		end = start + arrSize / 2;
+// 	} else 
+// 	{
+// 		start = arrSize / 2;
+// 		end = arrSize;
+// 	}
 
-	int sum = 0;
-	int i;
-	for (i = start; i < end; i++)
-		sum += array[i];
+// 	int sum = 0;
+// 	int i;
+// 	for (i = start; i < end; i++)
+// 		sum += array[i];
 
-	wait(NULL);
-	printf("Calculated partial sum: %d\n", sum);
+// 	wait(NULL);
+// 	printf("Calculated partial sum: %d\n", sum);
 	
-	if (id == 0)
-	{
-		close(fd[0]);
-		write(fd[1], &sum, sizeof(sum));
-		close(fd[1]);	 
-	} 
-		else 
-	{
-		int sumFromChild;
-		close(fd[1]);
-		read(fd[0], &sumFromChild, sizeof(sumFromChild));
-		close(fd[0]);
+// 	if (id == 0)
+// 	{
+// 		close(fd[0]);
+// 		write(fd[1], &sum, sizeof(sum));
+// 		close(fd[1]);	 
+// 	} 
+// 		else 
+// 	{
+// 		int sumFromChild;
+// 		close(fd[1]);
+// 		read(fd[0], &sumFromChild, sizeof(sumFromChild));
+// 		close(fd[0]);
 
-		int totalSum = sum + sumFromChild;
-		printf("Total sum is %d\n", totalSum);
-	}
+// 		int totalSum = sum + sumFromChild;
+// 		printf("Total sum is %d\n", totalSum);
+// 	}
 	
-	return (0);
+// 	return (0);
+// }
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+float A, B, C;
+
+float cubeWidth = 20;
+int width = 160, height = 44;
+float zBuffer[160 * 44];
+char buffer[160 * 44];
+int backgroundASCIICode = '.';
+int distanceFromCam = 100;
+float horizontalOffset;
+float K1 = 40;
+
+float incrementSpeed = 0.6;
+
+float x, y, z;
+float ooz;
+int xp, yp;
+int idx;
+
+float calculateX(int i, int j, int k) {
+  return j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) +
+         j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C);
 }
 
-// Just to update my git 
+float calculateY(int i, int j, int k) {
+  return j * cos(A) * cos(C) + k * sin(A) * cos(C) -
+         j * sin(A) * sin(B) * sin(C) + k * cos(A) * sin(B) * sin(C) -
+         i * cos(B) * sin(C);
+}
+
+float calculateZ(int i, int j, int k) {
+  return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);
+}
+
+void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch) {
+  x = calculateX(cubeX, cubeY, cubeZ);
+  y = calculateY(cubeX, cubeY, cubeZ);
+  z = calculateZ(cubeX, cubeY, cubeZ) + distanceFromCam;
+
+  ooz = 1 / z;
+
+  xp = (int)(width / 2 + horizontalOffset + K1 * ooz * x * 2);
+  yp = (int)(height / 2 + K1 * ooz * y);
+
+  idx = xp + yp * width;
+  if (idx >= 0 && idx < width * height) {
+    if (ooz > zBuffer[idx]) {
+      zBuffer[idx] = ooz;
+      buffer[idx] = ch;
+    }
+  }
+}
+
+int main() {
+  printf("\x1b[2J");
+  while (1) {
+    memset(buffer, backgroundASCIICode, width * height);
+    memset(zBuffer, 0, width * height * 4);
+    cubeWidth = 20;
+    horizontalOffset = -2 * cubeWidth;
+    // first cube
+    for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
+      for (float cubeY = -cubeWidth; cubeY < cubeWidth;
+           cubeY += incrementSpeed) {
+        calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
+        calculateForSurface(cubeWidth, cubeY, cubeX, '$');
+        calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
+        calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
+        calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
+        calculateForSurface(cubeX, cubeWidth, cubeY, '+');
+      }
+    }
+    cubeWidth = 10;
+    horizontalOffset = 1 * cubeWidth;
+    // second cube
+    for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
+      for (float cubeY = -cubeWidth; cubeY < cubeWidth;
+           cubeY += incrementSpeed) {
+        calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
+        calculateForSurface(cubeWidth, cubeY, cubeX, '$');
+        calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
+        calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
+        calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
+        calculateForSurface(cubeX, cubeWidth, cubeY, '+');
+      }
+    }
+    cubeWidth = 5;
+    horizontalOffset = 8 * cubeWidth;
+    // third cube
+    for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
+      for (float cubeY = -cubeWidth; cubeY < cubeWidth;
+           cubeY += incrementSpeed) {
+        calculateForSurface(cubeX, cubeY, -cubeWidth, '@');
+        calculateForSurface(cubeWidth, cubeY, cubeX, '$');
+        calculateForSurface(-cubeWidth, cubeY, -cubeX, '~');
+        calculateForSurface(-cubeX, cubeY, cubeWidth, '#');
+        calculateForSurface(cubeX, -cubeWidth, -cubeY, ';');
+        calculateForSurface(cubeX, cubeWidth, cubeY, '+');
+      }
+    }
+    printf("\x1b[H");
+    for (int k = 0; k < width * height; k++) {
+      putchar(k % width ? buffer[k] : 10);
+    }
+
+    A += 0.05;
+    B += 0.05;
+    C += 0.01;
+    usleep(8000 * 2);
+  }
+  return 0;
+}
