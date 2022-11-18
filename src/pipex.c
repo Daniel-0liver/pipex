0748d9abc2c613d47be37e0a902d2b7bf07de38f
Daniel-0liver <dtoliverbr@gmail.com>
2022-11-18 22:19:08 +0000

    execve

--- src/pipex.c
+++ src/pipex.c
@@ -14,37 +14,14 @@
 
 int main(void)
 {
-        int     fd[2], nbytes;
-        pid_t   childpid;
-        char    string[] = "Hello, world!\n";
-        char    readbuffer[80];
+        char    *argv[] = {"/bin/ls", "-l", NULL};
 
-        pipe(fd);
+        int val = execve(argv[0], argv, NULL);
         
-        if((childpid = fork()) == -1)
-        {
-                perror("fork");
-                exit(1);
-        }
+        if (val -- -1)
+                perror("error");
 
-        if(childpid == 0)
-        {
-                /* Child process closes up input side of pipe */
-                close(fd[0]);
+        printf("Done with ececve\n");
 
-                /* Send "string" through the output side of pipe */
-                write(fd[1], string, (strlen(string)+1));
-                exit(0);
-        }
-        else
-        {
-                /* Parent process closes up output side of pipe */
-                close(fd[1]);
-
-                /* Read in a string from the pipe */
-                nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
-                printf("Received string: %s", readbuffer);
-        }
-        
         return(0);
 }
