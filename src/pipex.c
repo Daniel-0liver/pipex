63356a9402fac236e69475227e4b103b158ab3a7
Daniel-0liver <dtoliverbr@gmail.com>
2022-11-30 04:10:44 +0000

    notes

--- src/pipex.c
+++ src/pipex.c
@@ -15,6 +15,7 @@
 int main(void)
 {
 	int fd;
+	int dupfile;
 	int pipes[2];
 	char path[] = "/usr/bin/ls";
 	char *argVec[] = {"ls", "-la", NULL};
@@ -22,12 +23,11 @@ int main(void)
 
 	pipe(pipes);
 	
+	int fd = open("file2", O_WRONLY | O_CREAT, 0777);
+	dupfile = dup2(fd, STDOUT_FILENO);
+	close(fd);
+	printf("Before the execve");
 	execve(path, argVec, envVec);
-	fd = open("file2", O_WRONLY);
-	dup2(fd, STDIN_FILENO);
-	close(0);
-	close(1);
-	write(pipes[2], "1", 1);
 
 	printf("Test");
 
